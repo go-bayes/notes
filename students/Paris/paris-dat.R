70cac33d17672725c7a0e25e12f70b9b24f890b9
Joseph Bulbulia <joseph.bulbulia@gmail.com>
2021-07-29 21:13:01 +1200

    scripts for students

---
+++ students/Paris/paris-dat.R
@@ -0,0 +1,40 @@
+# 2021 
+# 489 for Paris Holden on Generational Anxiety
+# libraries 
+
+# libraries 
+library("tidyverse")
+library("here")
+library("ggplot")
+library("naniar")
+
+# read NZAVS data
+df <- readRDS(here::here("data_raw","df"))
+
+# look at the dataframe
+dplyr::glimpse(df)
+
+# select variables that  
+df$
+# create 
+pdf <- df %>% 
+  select(Id, 
+         YearMeasured, 
+         years,  
+         Wave, 
+         BELONG, 
+         SUPPORT, 
+         Hours.Friends,
+         Hours.Family,
+         SWB.SoC01, # neighbourhood community,
+        # SELF.ESTEEM jb to get
+        # PERFECTIONISM
+        # Emotional regulation
+         PWI, 
+         LIFESAT,
+         KESSLER6sum,
+         Hours.CompGames,
+         Hours.SocialMedia,
+         Hours.Internet)
+
+
