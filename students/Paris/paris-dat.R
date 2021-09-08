b20fa99e3f1d1ef8bbcb3afa40a43de66382f0f1
Joseph Bulbulia <joseph.bulbulia@gmail.com>
2021-09-08 16:11:04 +1200

    update notes

--- students/Paris/paris-dat.R
+++ students/Paris/paris-dat.R
@@ -8,42 +8,42 @@ library("tidyverse")
 library("here")
 library("ggplot")
 library("naniar")
-
-# read NZAVS data
-df <- readRDS(here::here("data_raw","df"))
-
-# look at the dataframe
-dplyr::glimpse(df)
-
-# select variables that  
-df$EthnicCats
-# create 
-pdf <- df %>% 
-  select(Id, 
-         YearMeasured, 
-         years,  
-         Wave, 
-         BELONG, 
-         SUPPORT, 
-         Hours.Friends,
-         Hours.Family,
-         SWB.SoC01, # neighbourhood community,
-         SELF.ESTEEM, #jb to get
-         PWI, 
-         LIFESAT,
-         KESSLER6sum,
-         Hours.CompGames,
-         Hours.SocialMedia,
-         Hours.Internet,
-         Age,
-         Male,
-         Relid,
-         Edu,
-         Partner,
-         EthnicCats
-         )
-saveRDS(pdf, here::here("data_raw", "pdf"))
-
-here()
-test <- readRDS(here::here("data_raw", "pdf"))
-head(test)
+# 
+# # read NZAVS data
+# df <- readRDS(here::here("data","paris_dat.rds"))
+# 
+# # look at the dataframe
+# dplyr::glimpse(df)
+# 
+# # select variables that  
+# 
+# # create 
+# pdf <- df %>% 
+#   select(Id, 
+#          YearMeasured, 
+#          years,  
+#          Wave, 
+#          BELONG, 
+#          SUPPORT, 
+#          Hours.Friends,
+#          Hours.Family,
+#          SWB.SoC01, # neighbourhood community,
+#          SELF.ESTEEM, #jb to get
+#          PWI, 
+#          LIFESAT,
+#          KESSLER6sum,
+#          Hours.CompGames,
+#          Hours.SocialMedia,
+#          Hours.Internet,
+#          Age,
+#          Male,
+#          Relid,
+#          Edu,
+#          Partner,
+#          EthnicCats
+#          )
+# saveRDS(pdf, here::here("data_raw", "pdf"))
+# 
+# here()
+# test <- readRDS(here::here("data_raw", "pdf"))
+# head(test)
