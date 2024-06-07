60ffb76e49f5b9ed088f61a323d9afa777823b4b
Joseph Bulbulia <joseph.bulbulia@gmail.com>
2024-06-07 12:30:33 +1200

    jakes simulation

--- simulations/sim-big-gods.R
+++ simulations/sim-big-gods.R
@@ -4,9 +4,13 @@ if (!require("tidyr")) install.packages("tidyr")
 if (!require("geepack")) install.packages("geepack")
 if (!require("naniar")) install.packages("naniar")
 
+library(margot)
+library(lmtp)
 library(dplyr)
 library(tidyr)
 library(geepack)
+library(table1)
+library(kableExtra)
 
 
 ## seed
@@ -14,7 +18,12 @@ set.seed(123)
 
 # define the number of cultures and the number of obs for each group
 n_cultures <- 200
-obs_counts <- c(rep(1, 10), rep(3, 190))  # some cultures have only one observation
+
+#obs_counts <- c(rep(1, 10), rep(3, 190))  # some cultures have only one observation
+
+# let's simplify so that we require that a culture must have at least 2 x observations
+obs_counts <- rep(3, 200)
+
 
 # init empty data frame
 data <- data.frame(id = integer(), year_measured = integer(), beliefs_big_gods = integer(), 
@@ -58,29 +67,49 @@ nrow(data)
 # view -- note this is in long format
 head(data)
 
-# hack to get the lead of social complexity as the correct value ( JAKE YOU WON'T NEED TO DO THIS)
-data <- data |>
+# hack to get the lead of social complexity as the correct value 
+data_selected <- data |>
   group_by(id) |>
   mutate(lead_social_complexity = social_complexity) |>
+  ungroup() |> 
+  droplevels() # safety
+
+head(data_selected)
+
+table(is.na( data_selected$lead_social_complexity))
+
+
+# make 10% of lead social complexity NA values, do this by "person time" of culture
+data_lead <- data_selected |>
+  group_by(id)  |>
+  mutate(
+    lead_social_complexity = case_when(
+      year_measured == 2 & runif(n()) < 0.1 ~ NA,
+      year_measured == 3 & runif(n()) < 0.15 ~ NA,
+      TRUE ~ lead_social_complexity
+    )
+  )  |>
   ungroup()
 
-nrow(data)
-
-# view
-head(data)
 
 
-# create the lost indicator -- JAKE YOU"LL NEED TO GET A CENSORING INDICATOR IN YOUR DATA 
-# convert your data to long format and do this. 
-data <- data |>
-  group_by(id) |>
-  mutate(lost = ifelse(n() == 1, 1, 0)) |>
+# check
+head(data_lead)
+
+#check
+table1::table1(data = data_lead, ~ as.factor(lead_social_complexity) | as.factor(year_measured))
+
+# make not lost indicator
+data_lead <- data_lead |>
+  group_by(year_measured) |>
+  mutate(not_lost = ifelse(!is.na(lead_social_complexity), 1, 0)) |>
   ungroup()
 
 # filter cases where beliefs_big_gods and social_complexity are 0 at the first observation
-filtered_data <- data |>
+filtered_data <- data_lead |>
   group_by(id) |>
   filter(beliefs_big_gods[1] == 0 & social_complexity[1] == 0) |>
+  mutate(wave  = year_measured) |> # for simplicity
   ungroup()
 
 
@@ -89,6 +118,16 @@ nrow(filtered_data)
 
 table(filtered_data$beliefs_big_gods)
 table(filtered_data$social_complexity)
+table(filtered_data$not_lost)
+
+hist(filtered_data$confounder)
+filtered_data
+head(filtered_data$year_measured)
+
+table1::table1(data = filtered_data, ~ as.factor(social_complexity)| wave)
+table1::table1(data = filtered_data, ~ as.factor(not_lost)| wave)
+
+
 
 
 # you can do something like this if needed to get leads and lags in your dataframe. 
@@ -185,3 +224,106 @@ summary(gee_msm)
 exp(coef(gee_msm)["beliefs_big_gods"])
 
 
+
+
+
+# USE THIS MODEL JAKE -----------------------------------------------------
+
+
+# lmtp model --------------------------------------------------------------
+
+# remove attributes
+dt_lmtp <- margot::remove_numeric_attributes(final_data)
+
+# get data in shape
+df_clean <- dt_lmtp |>
+  mutate(id_f = as.character(id),
+         not_lost_f = as.character(not_lost),
+         wave = as.character(year_measured)) |> 
+  select(
+    id, id_f, wave,lag_confounder_impute, beliefs_big_gods, not_lost_f, lead_social_complexity
+  ) # noted that "not_lost_f" must come before the outcomes
+
+
+
+# check data structure
+head(df_clean)
+table(df_clean$not_lost_f)
+
+
+policy_no_big_gods <- function(data, trt) {
+  ifelse(data[[trt]] != 0, 0,  data[[trt]])
+}
+
+policy_yes_big_gods <- function(data, trt) {
+  ifelse(data[[trt]] != 1, 1,  data[[trt]])
+}
+
+# confounder
+L <- c("lag_confounder_impute", "wave")
+# lmtp model (not nec valid standard errors for small sample)
+
+
+fit_no_beliefs <- lmtp::lmtp_tmle(
+    data = df_clean,
+    trt = "beliefs_big_gods",
+    outcome = "lead_social_complexity",
+    baseline = L,
+    cens = "not_lost_f", 
+    shift = policy_no_big_gods,
+    mtp = TRUE,
+    outcome_type = "binomial",
+    learners_outcome = c("SL.ranger",  "SL.glmnet"),  # small sample properties of ranger and glmnet are OK. not sure of the others
+    learners_trt = c("SL.ranger",  "SL.glmnet"),
+    id = "id_f",
+    folds = 10,
+    n_cores = 5                                     
+  )
+
+
+# check
+fit_no_beliefs
+
+
+
+
+fit_yes_beliefs <- lmtp::lmtp_tmle(
+  data = df_clean,
+  trt = "beliefs_big_gods",
+  outcome = "lead_social_complexity",
+  baseline = L,
+  cens = "not_lost_f", 
+  shift = policy_yes_big_gods,
+  mtp = TRUE,
+  outcome_type = "binomial",
+  learners_outcome = c("SL.ranger",  "SL.glmnet"),  # small sample properties of ranger and glmnet are OK. not sure of the others
+  learners_trt = c("SL.ranger",  "SL.glmnet"),
+  id = "id_f",
+  folds = 10,
+  n_cores = 5                               
+)
+
+#check
+contrast <- lmtp::lmtp_contrast(fit_yes_beliefs, ref = fit_no_beliefs, type = "rr")
+
+# get results using margot package
+result <- margot_lmtp_evalue(contrast, scale = "RR", new_name = "Causal Effect of Beliefs in Big Gods on Social Complexity (risk)")
+
+# result
+result
+
+
+# interpretation
+interpretation <- margot::margot_interpret_table(result, causal_scale = "RR", estimand = "ATE")
+interpretation
+
+# table for your manuscript
+table <- margot::margot_lmtp_evalue(contrast, scale = "RD", new_name = "Causal Effect of Beliefs in Big Gods on Social Complexity (risk)")
+
+# markdown table
+table |> 
+  kbl(format = "markdown")
+
+# latex
+table |> 
+  kbl(format = "latex")
