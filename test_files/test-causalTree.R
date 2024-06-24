9ada9682764ea9c77b3949a9d55c24bfc6d6190e
Joseph Bulbulia <joseph.bulbulia@gmail.com>
2024-06-25 09:46:09 +1200

    causal tree

--- test_files/test-causalTree.R
+++ test_files/test-causalTree.R
@@ -1,9 +1,24 @@
 # test using causalTree
-library(margot)
 
+# documentation
+https://github.com/susanathey/causalTree
+
+# install
 devtools::install_github("susanathey/causalTree")
 
+# call library
 library(causalTree)
 
-df_long <- df_nz |>
+tree <- causalTree(y~ x1 + x2 + x3 + x4, data = simulation.1, treatment = simulation.1$treatment,
+                   split.Rule = "CT", cv.option = "CT", split.Honest = T, cv.Honest = T, split.Bucket = F, 
+                   xval = 5, cp = 0, minsize = 20, propensity = 0.5)
+
+head(tree)
+
+opcp <- tree$cptable[,1][which.min(tree$cptable[,4])]
+
+opfit <- prune(tree, opcp)
+
+rpart.plot(opfit)
+
 
